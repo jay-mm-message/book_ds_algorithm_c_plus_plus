9c586a265542c512b58771f3770bc14b0c33d321
jay-mm-message <jay.mm.message@gmail.com>
2025-09-13 10:46:29 +0800

    Add a exmple for test case

--- ch2_array/leetcode/70_Climbing_Stairs.cpp
+++ ch2_array/leetcode/70_Climbing_Stairs.cpp
@@ -24,6 +24,25 @@ int non_recursive_fib(int n) {
 	return ret;
 }
 
+int recursive_test(int fn) {
+	if (fn == 1) return 1;
+	if (fn == 0) return 1;
+	return recursive_test(fn - 1) + recursive_test(fn - 2);
+}
+
+int non_recursive_test(int n) {
+	if (n == 1) return 1;
+	if (n == 0) return 1;
+
+	int ret = 0, a = 1, b = 1;
+	for(int i = 2 ; i <= n ; ++i) {
+		ret = a + b;
+		a = b;
+		b = ret;
+	}
+	return ret;
+}
+
 void result(int n) {
 	cout << "recursive: ";
 	cout << "fib(" << n << "): ";
@@ -32,6 +51,14 @@ void result(int n) {
 	cout << "non recursive: ";
 	cout << "fib(" << n << "): ";
 	cout << non_recursive_fib(n) << endl;
+
+	cout << "recursive (test): ";
+	cout << "fin(" << n << "): ";
+	cout << recursive_test(n) << endl;
+
+	cout << "non_recursive (test): ";
+	cout << "fin(" << n << "): ";
+	cout << non_recursive_test(n) << endl;
 }
 
 int main(void) {
