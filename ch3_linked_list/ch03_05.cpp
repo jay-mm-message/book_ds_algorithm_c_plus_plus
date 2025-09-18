5f69b731bc9551b91b1af458b5353a5b9710428a
jay-mm-message <jay.mm.message@gmail.com>
2025-09-18 15:50:16 +0800

    Add a example of ch03_05.cpp

---
+++ ch3_linked_list/ch03_05.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class st_info {
+	public:
+		int id;
+		int score;
+		string name;
+		st_info *next;
+	public:
+		st_info(int id, int score, string name) {
+			this->id = id;
+			this->score = score;
+			this->name = name;
+			this->next = nullptr;
+		}
+		~st_info() {
+			cout << "After freeing id: " << this->id << endl;
+		}
+		void get_st_info() {
+			cout << "id: " << this->id;
+			cout << ", score: " << this->score;
+			cout << ", name: " << this->name;
+			cout << endl;
+		}
+};
+
+typedef st_info node;
+typedef node *list;
+
+void print_st_info_list(list orig) {
+	if (orig == nullptr) return;
+
+	list ptr = orig;
+	while(ptr != nullptr) {
+		ptr->get_st_info();
+		ptr = ptr->next;
+	}
+	cout << "Display all elements in st_information.\n" << endl;
+}
+
+void free_st_info_list(list &orig) {
+	if (orig == nullptr) return;
+
+	list ptr = orig;
+	while(ptr != nullptr) {
+		list tmp = ptr;
+		ptr = ptr->next;
+		delete tmp;
+	}
+	cout << "After freeing all elements in st_information.\n" << endl;
+}
+
+void remove_specified_position(int index, list &orig) {
+	if (orig == nullptr) return;
+
+	list ptr = orig;
+	while(ptr != nullptr) {
+		if (ptr->next->id == index) {
+			break;
+		}
+		ptr = ptr->next;
+	}
+
+	list tmp = ptr->next;
+	ptr->next = ptr->next->next;
+
+	ptr = ptr->next;
+	while(ptr != nullptr) {
+		ptr->id -= 1;
+		ptr = ptr->next;
+	}
+
+	cout << "Remove the specified position: " << tmp->id;
+	cout << ", score: " << tmp->score;
+	cout << ", name: " << tmp->name;
+	cout << "\n" << endl;
+	delete tmp;
+
+	cout << "After deleting an the specified position: " << index << endl;
+}
+
+int main() {
+	const int size = 5;
+	int st_no[size] = {1, 2, 3, 4 , 5};
+	int st_score[size] = {85, 95, 68, 72, 79};
+	string st_name[size] = {"andy", "michael", "may", "tom", "daniel"};
+
+	list head = nullptr, ptr = head;
+	for(size_t i = 0 ; i < size ; ++i) {
+		list new_node = new node(st_no[i], st_score[i], st_name[i]);
+		if (head != nullptr) {
+			ptr->next = new_node;
+			ptr = ptr->next;
+		} else {
+			head = new_node;
+			ptr = head;
+		}
+	}
+
+	print_st_info_list(head);
+	remove_specified_position(2, head);
+	print_st_info_list(head);
+	return 0;
+}
